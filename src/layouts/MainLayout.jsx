b353400f282b59381b4844b84b5d524d0dd2fde2
unknown <anggiprayoga930@gmail.com>
2025-07-31 09:10:18 +0700

    Feat: Add MainLayout component with Navbar and Footer

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,17 @@
+// import React from 'react';
+import Navbar from '../components/Navbar.jsx';
+import Footer from '../components/Footer.jsx';
+
+const MainLayout = ({ children }) => {
+  return (
+    <div className="font-mulish bg-white">
+      <Navbar />
+      <main>
+        {children}
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default MainLayout;
