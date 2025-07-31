b1802a0d5a7fbe51fe379bacb79f49ebdd9a37fa
unknown <anggiprayoga930@gmail.com>
2025-07-31 09:01:37 +0700

    Refactor: Update imports and component usage in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,29 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+// import { BrowserRouter } from 'react-router-'
 import './index.css'
-// import App from './App.jsx'
-import TickitzLogin from './components/Login.jsx'
-import TickitzRegister from './components/Register.jsx'
-import MovieWebsite from './components/Movie.jsx'
+import App from './App.jsx'
+// import TickitzLogin from './components/Login.jsx'
+// import TickitzRegister from './components/Register.jsx'
+import MovieWebsite from './pages/Movie.jsx'
+import MainLayout from './layouts/MainLayout.jsx'
+import LandingPage from './pages/LandingPage.jsx'
+import OrderPage from './pages/orderpage.jsx'
+import Payment from './pages/Payment.jsx'
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    {/* <MainLayout /> */}
     {/* <TickitzLogin /> */}
     {/* <TickitzRegister /> */}
     {/* Uncomment the line below to use the App component */}
     {/* <App /> */}
-    <MovieWebsite />
+    {/* <MovieWebsite /> */}
+    {/* <Router /> */}
+    {/* <LandingPage /> */}
+    <OrderPage />
+    {/* <Payment /> */}
   </StrictMode>
 )
